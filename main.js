8175ca6cbf6d15f351a4c0022569e1de7eafdbc7
Corwyn Giles <corwyngiles@gmail.com>
2024-02-26 18:03:19 -0700

    Implemented grabbing values from text boxes

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ function submitReview(){
 
 }
 function login(){
-
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
 }
 function register(){
-    
+
 }
