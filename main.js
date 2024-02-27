46800406d5320edd0bf4138d1fea07de4b5b73d3
Corwyn Giles <corwyngiles@gmail.com>
2024-02-26 18:09:31 -0700

    created show message function, added error checking on login submissions

--- main.js
+++ main.js
@@ -6,7 +6,19 @@ function login(){
     const password = document.getElementById("password").value;
     document.getElementById("username").value = "";
     document.getElementById("password").value = "";
+    if(!username || !password){
+        const x = "Either your Username or Password are not filled in. Please try again.";
+        showMessage(x);
+    } else {
+        //check login database, send needed message depending on pass/fail. If pass change welcome guest to welcome username, and add logout button
+    }
 }
 function register(){
 
+}
+function showMessage(x){
+
+}
+function logout(){
+    
 }
