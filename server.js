8118a5bde535ad48bf375731dd0b01cf3b6312b3
Corwyn Giles <corwyngiles@gmail.com>
2024-03-08 15:42:19 -0700

    added check auth service for backend

--- server.js
+++ server.js
@@ -10,17 +10,16 @@ app.post('/register', (req, res, next) => {
     const password = req.password;
     
     //return authtoken if succesful, error if fail
-    if(checkName(username)){
+    if(checkUser(username) === null){
         users.push({username: username, password: password});
-        const authToken = String(crypto.randomUUID());
-        tokens.push({username: username, token: authToken});
+        const authToken = tokenGenerator(username);
         res.status(200).send({token: authToken});
     }else {
         res.status(401).send();
     }
   });
 
-function checkName(x) {
+function checkUser(x) {
     for (i in users){
         if(i.username === x){
             return i.password;
@@ -36,7 +35,7 @@ function checkReview(x,y){
     }
     return null;
 }
-function checkAuth(w,x){
+function checkAuth(w){
     for (i in tokens){
         if(i.token === w){
             return i.username;
@@ -46,28 +45,38 @@ function checkAuth(w,x){
 }
 
 //Middleware for getting x users review score of game y
-app.get('score', (req, res, next) => {
+app.get('/score', (req, res, next) => {
 
 });
 //Middleware for submitting x users review score of game y
-app.post('score', (req, res, next) => {
+app.post('/score', (req, res, next) => {
     
 });
 //Middleware for updating x users review score of game y
-app.put('score', (req, res, next) => {
+app.put('/score', (req, res, next) => {
     
 });
 //Middleware for logout
-app.delete('user', (req, res, next) => {
+app.delete('/user', (req, res, next) => {
     
 });
 //Middleware for getting number of reviews and total score of game y
-app.get('review', (req, res, next) => {
+app.get('/review', (req, res, next) => {
     
 });
 //Middleware for logging in user x with password z
-app.post('user', (req, res, next) => {
+app.post('/user', (req, res, next) => {
     
+});
+//middleware for checking if a authtoken is valid
+app.get('/user', (req, res, next) => {
+    const token = req.token;
+    const name = checkAuth(token);
+    if(name != null){
+        res.status(200).send({username: name});
+    } else {
+        res.status(401).send();
+    }
 });
 function tokenGenerator(username){
     const uuid = String(crypto.randomUUID());
