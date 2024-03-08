39db459aceaee783f4b4711847086935b8fb397a
Corwyn Giles <corwyngiles@gmail.com>
2024-03-08 15:19:54 -0700

    created database arrays, added check functions for those arrays

--- server.js
+++ server.js
@@ -1,19 +1,51 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+let users = [];
+let tokens = [];
+let reviews = [];
+//Middleware for registering x user with password z
 app.post('/register', (req, res, next) => {
     const username = req.name;
     const password = req.password;
-    //add username and password to the database
+    
     //return authtoken if succesful, error if fail
-    const authToken = String(crypto.randomUUID());
-    //add authtoken into auth database
-    res.status(200).send({token: authToken});
+    if(checkName(username)){
+        users.push({username: username, password: password});
+        const authToken = String(crypto.randomUUID());
+        tokens.push({username: username, token: authToken});
+        res.status(200).send({token: authToken});
+    }else {
+        res.status(401).send();
+    }
   });
-const port = 3000;
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
-});
+
+function checkName(x) {
+    for (i in users){
+        if(i.username === x){
+            return true;
+        }
+    }
+    return false;
+}
+function checkReview(x,y){
+    for (i in reviews){
+        if(i.username === x && i.gameID === y){
+            return i.score;
+        } else {
+            return null;
+        }
+    }
+}
+function checkAuth(w,x){
+    for (i in tokens){
+        if(i.token === w){
+            return i.username;
+        } else {
+            return null;
+        }
+    }
+}
 
 //Middleware for getting x users review score of game y
 app.get('score', (req, res, next) => {
@@ -38,4 +70,13 @@ app.get('review', (req, res, next) => {
 //Middleware for logging in user x with password z
 app.post('user', (req, res, next) => {
     
+});
+function tokenGenerator(username){
+    const uuid = String(crypto.randomUUID());
+    localStorage.setItem(uuid,username);
+    localStorage.setItem("authToken", uuid);
+}
+const port = 3000;
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
 });
